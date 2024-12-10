51c04da3ce5193d697564228cf6d1a493460b6fd
Yanjin Chen <yanjin.chen@student.tdsb.on.ca>
2024-12-10 11:30:38 -0500

    firebase setup

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
-
+const firebaseConfig = {
+    apiKey: "AIzaSyCeUeV8WYFkU_VSE0faSnOgzs1tc4cj5Cw",
+    authDomain: "summative-11703.firebaseapp.com",
+    projectId: "summative-11703",
+    storageBucket: "summative-11703.firebasestorage.app",
+    messagingSenderId: "50015309084",
+    appId: "1:50015309084:web:ea0de1bec1f7a38e28fed9"
+  };
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
