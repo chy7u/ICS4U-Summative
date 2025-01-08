2d5df13024392d6d3baef018c6f9d1110a3f20a9
Yanjin Chen <yanjin.chen@student.tdsb.on.ca>
2025-01-08 12:00:58 -0500

    idk what i did today

--- src/views/LoginView.jsx
+++ src/views/LoginView.jsx
@@ -1,41 +1,57 @@
 import React from 'react';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useStoreContext } from "../context/GlobalState";
+import { signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { auth } from '../firebase';
 import './LoginView.css';
 
 function LoginView() {
-    const { password, email, loggedIn, setLoggedIn } = useStoreContext();
-    const enteredPassword = useRef("");
+    const { setUser } = useStoreContext();
+    //const enteredPassword = useRef("");
     const enteredEmail = useRef("");
+    const [password, setPassword] = useState('');
     const navigate = useNavigate();
 
-    function login(event) {
+    async function loginByEmail(event) {
         event.preventDefault();
-        if (enteredPassword.current.value === password
-            && enteredEmail.current.value === email
-        ) {
-            setLoggedIn(true);
-            setTimeout(() => {
-                navigate('/movies');
-            }, 0);
-        } else {
-            alert("Wrong email or password!");
-        }
+
+        try {
+            const user = (await signInWithEmailAndPassword(auth, enteredEmail.current.value, password)).user;
+            navigate('/movies');
+            setUser(user);
+          } catch (error) {
+            console.log(error);
+            alert("Error signing in!");
+          }        
     }
 
-    console.log(loggedIn);
+//    function login(event) {
+//        event.preventDefault();
+//        if (enteredPassword.current.value === password
+//            && enteredEmail.current.value === email
+//        ) {
+//            setLoggedIn(true);
+//            setTimeout(() => {
+//                navigate('/movies');
+//            }, 0);
+//        } else {
+//            alert("Wrong email or password!");
+//        }
+//    }
+//
+//    console.log(loggedIn);
 
     return (
         <div className="login-container">
             <div className="form-container">
                 <h2>Login</h2>
-                <form onSubmit={(event) => {login(event)}}>
+                <form onSubmit={(event) => { loginByEmail(event) }}>
                     <label htmlFor="email">Email:</label>
                     <input type="email" id="email" name="email" ref={enteredEmail} required/>
 
                     <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" name="password" ref={enteredPassword} required/>
+                    <input type="password" id="password" name="password" value={password} onChange={(event) => { setPassword(event.target.value) }} required/>
 
                     <button type="submit" className="login-button">Login</button>
                 </form>
